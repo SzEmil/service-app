5361ff5ae1af5a1d75d891b95368409bc7b0acfe
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2023-07-15 20:57:09 +0200

    first component nextjs SSR

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import UserData from './user-data';
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <UserData userData={[]} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ client/pages/user-data.tsx
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import css from "../styles/user-data.module.css"
+type UserDataType = {
+  _id: string | null;
+  email: string | null;
+  subscription: string | null;
+};
+
+type UserDataProps = {
+  userData: UserDataType[];
+};
+
+const UserData = ({ userData }: UserDataProps) => {
+
+  return (
+    <div>
+      {userData.map(user => (
+        <div key={user._id}>
+          <h1 className={css.title}>{user._id}</h1>
+          <p>{user.email}</p>
+          <p>{user.subscription}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export async function getServerSideProps() {
+  try {
+    const response = await axios.get('http://localhost:3001/api/users');
+    const data = response.data;
+    const userData = data.ResponseBody.users;
+
+    return {
+      props: {
+        userData,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      props: {
+        userData: [],
+      },
+    };
+  }
+}
+
+export default UserData;
